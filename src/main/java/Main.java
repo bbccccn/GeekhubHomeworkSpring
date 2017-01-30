1fc2c7fcc6c0dd92bf7394fe60188ad21989b9b5
bbcccn <landex4@gmail.com>
2017-01-30 08:41:38 +0200

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
+import Entitites.Car;
+import Entitites.Configs.Config;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext ctx;
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+
+        Car car = ctx.getBean(Car.class);
+        System.out.println("Testing java based Spring config...");
+        System.out.println("Engine capacity: " + car.getEngine().getEngineCapacity());
+        System.out.println("Wheel name: " + car.getWheels().get(0).getTyres().getName());
     }
 }
